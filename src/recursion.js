2607504a5736ec67fc0385b9124af885bae3dff5
Joseph Casey <joecasey22@gmail.com>
2021-09-15 22:46:21 -0400

    Completed functions 1 - 21

--- src/recursion.js
+++ src/recursion.js
@@ -102,7 +102,6 @@ var exponent = function(base, exp) {
   } else {
     return parseFloat(((1 / base) * exponent(base, next)).toFixed(5));
   }
-
 };
 
 // 8. Determine if a number is a power of two.
@@ -200,6 +199,19 @@ var divide = function(x, y) {
 // http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
 // https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
 var gcd = function(x, y) {
+  if (x < 0 || y < 0) {
+    return null;
+  }
+
+  if (x % y === 0) {
+    return y;
+  }
+
+  if (y % x === 0) {
+    return x;
+  }
+
+  return gcd(y, x % y);
 };
 
 // 15. Write a function that compares each character of two strings and returns true if
@@ -207,21 +219,52 @@ var gcd = function(x, y) {
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function(str1, str2) {
+  if (str1.length === 0 && str2.length === 0) {
+    return true;
+  }
+
+  if (str1[0] !== str2[0]) {
+    return false;
+  }
+
+  return compareStr(str1.substring(1), str2.substring(1));
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter
 // occupies an index of the array.
 var createArray = function(str) {
+  var results = [];
+  if (str.length === 0) {
+    return results;
+  }
+
+  results.push(str[0]);
+  return results.concat(createArray(str.substring(1)));
 };
 
 // 17. Reverse the order of an array
 var reverseArr = function(array) {
+  var results = [];
+  if (array.length === 0) {
+    return results;
+  }
+
+  results.push(array[array.length - 1]);
+  array.pop();
+  return results.concat(reverseArr(array));
 };
 
 // 18. Create a new array with a given value and length.
 // buildList(0,5) // [0,0,0,0,0]
 // buildList(7,3) // [7,7,7]
 var buildList = function(value, length) {
+  var list = [];
+  if (length === 0) {
+    return list;
+  }
+
+  list.push(value);
+  return list.concat(buildList(value, length - 1));
 };
 
 // 19. Implement FizzBuzz. Given integer n, return an array of the string representations of 1 to n.
@@ -230,17 +273,55 @@ var buildList = function(value, length) {
 // For numbers which are multiples of both three and five, output “FizzBuzz” instead of the number.
 // fizzBuzz(5) // ['1','2','Fizz','4','Buzz']
 var fizzBuzz = function(n) {
+  var results = [];
+  if (n === 0) {
+    return results;
+  }
+
+  var value = '';
+  if (n % 3 === 0) {
+    value += 'Fizz';
+  }
+
+  if (n % 5 === 0) {
+    value += 'Buzz';
+  }
+
+  if (value === '') {
+    value += n;
+  }
+
+  return results.concat(fizzBuzz(n - 1), value);
 };
 
 // 20. Count the occurrence of a value in a list.
 // countOccurrence([2,7,4,4,1,4], 4) // 3
 // countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
 var countOccurrence = function(array, value) {
+  var count = 0;
+  if (array.length === 0) {
+    return 0;
+  }
+
+  if (array[0] === value) {
+    count++;
+  }
+
+  array.shift();
+  return count + countOccurrence(array, value);
 };
 
 // 21. Write a recursive version of map.
 // rMap([1,2,3], timesTwo); // [2,4,6]
 var rMap = function(array, callback) {
+  var copy = [...array];
+  var results = [];
+  if (copy.length === 0) {
+    return results;
+  }
+
+  results.push(callback(copy.shift()));
+  return results.concat(rMap(copy, callback));
 };
 
 // 22. Write a function that counts the number of times a key occurs in an object.
